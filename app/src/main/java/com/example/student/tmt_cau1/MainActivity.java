3f01ceb7158b772d4ecb5d250389b3dd95433352
chemgiohaiz <chemgiohaiz@gmail.com>
2019-09-09 17:09:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/student/tmt_cau1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.student.tmt_cau1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button bt_dangnhap;
+        final CheckBox ckb_luu;
+        final TextView tv_thongbao;
+        bt_dangnhap=(Button)findViewById(R.id.button_dangnhap);
+        ckb_luu=(CheckBox)findViewById(R.id.ckbluu);
+        tv_thongbao=(TextView)findViewById(R.id.textView_thongbao);
+
+        bt_dangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ckb_luu.isChecked()){
+                    tv_thongbao.setText("Chào mừng bạn đăng nhập vào hệ thống, thông tin của bạn đã được luu");
+                }else{
+                    tv_thongbao.setText("Chào mừng bạn đăng nhập vào hệ thống, thông tin của bạn không được luu");
+                }
+            }
+        });
+    }
+}
