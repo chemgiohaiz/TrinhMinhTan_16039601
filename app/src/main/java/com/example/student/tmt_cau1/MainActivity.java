43b1b7f77706ff8aa87d8b0befc04ac2cffecae2
chemgiohaiz <55051620+chemgiohaiz@users.noreply.github.com>
2019-09-15 00:29:59 +0700

    First Commit

--- app/src/main/java/com/example/student/tmt_cau1/MainActivity.java
+++ app/src/main/java/com/example/student/tmt_cau1/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    AlertDialog.Builder alertdialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         Button bt_dangnhap,bt_thoat;
         final CheckBox ckb_luu;
         final TextView tv_thongbao;
-        alertdialog = new AlertDialog.Builder(this);
         bt_dangnhap=(Button)findViewById(R.id.button_dangnhap);
         ckb_luu=(CheckBox)findViewById(R.id.ckbluu);
         tv_thongbao=(TextView)findViewById(R.id.textView_thongbao);
@@ -33,32 +31,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        bt_thoat=(Button)findViewById(R.id.button_thoat);
-        bt_thoat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                alertdialog.setTitle("Thông báo");//bị gì mẹ r
-                alertdialog.setMessage("Bạn có muốn thoát không");
-                alertdialog.setCancelable(false);
-                alertdialog.setPositiveButton("Có", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-
-                    }
-                });
-//sao để đăng xuất git t ra
-                alertdialog.setNegativeButton("Không", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
-
-                alertdialog.show();
-            }
-
-        });
+        
     }
 }
